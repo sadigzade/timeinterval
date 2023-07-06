2549534ebad9bbefe11b14116eb27018c14411de
Khikmet Sadigzade <sadigzade.hi@mail.ru>
2023-07-06 21:06:55 +0300

    Made a rotating circle with the current value

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./App.module.css";
+import CentetCircle from "../CentetCircle/CentetCircle";
 
-function App() {
-  return <h1>App</h1>;
-}
+const App = () => {
+  const [currentPoint, setCurrentPoint] = useState(1);
+  const [circleDeg, setCircleDeg] = useState(-60);
+  const [pointDeg, setPointDeg] = useState(60);
+
+  const onPointClick = (point: number) => {
+    setCurrentPoint(point);
+  };
+
+  const onPrev = () => {
+    if (currentPoint === 1) return;
+    setCurrentPoint(currentPoint - 1);
+    setCircleDeg(circleDeg - 60);
+    setPointDeg(pointDeg + 60);
+  };
+
+  const onNext = () => {
+    if (currentPoint === 6) return;
+    setCurrentPoint(currentPoint + 1);
+    setCircleDeg(circleDeg + 60);
+    setPointDeg(pointDeg - 60);
+  };
+
+  return (
+    <div className="container">
+      <div className={styles.App}>
+        <div className={styles.Content}>
+          <div></div>
+          <div className={styles.CircleControl}>
+            <span>0{currentPoint}/06</span>
+            <div className={styles.CirlceArrows}>
+              <button
+                className={`${styles.CircleArrowPrev} ${currentPoint === 1 ? styles.disabled : ""}`}
+                onClick={onPrev}
+              >
+                <svg
+                  width="10"
+                  height="14"
+                  viewBox="0 0 10 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M8.49988 0.750001L2.24988 7L8.49988 13.25"
+                    stroke="#42567A"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </button>
+              <button
+                className={`${styles.CircleArrowNext} ${currentPoint === 6 ? styles.disabled : ""}`}
+                onClick={onNext}
+              >
+                <svg
+                  width="10"
+                  height="14"
+                  viewBox="0 0 10 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M8.49988 0.750001L2.24988 7L8.49988 13.25"
+                    stroke="#42567A"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className={styles.Carusel}></div>
+        </div>
+        <CentetCircle
+          currentPoint={currentPoint}
+          circleDeg={circleDeg}
+          pointDeg={pointDeg}
+          onPointClick={onPointClick}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/CentetCircle/CentetCircle.tsx
@@ -0,0 +1,51 @@
+import { FC, useState } from "react";
+import styles from "./CentetCircle.module.css";
+
+const D = 530;
+
+type CentetCircleProps = {
+  currentPoint: number;
+  circleDeg: number;
+  pointDeg: number;
+  onPointClick: (point: number) => void;
+};
+
+const CentetCircle: FC<CentetCircleProps> = ({
+  currentPoint,
+  circleDeg,
+  pointDeg,
+  onPointClick,
+}) => {
+  return (
+    <div className={styles.CentetCircle}>
+      <ul
+        className={styles.CirclePoints}
+        style={{
+          transform: `translate(-50%, -50%) rotate(${circleDeg}deg)`,
+        }}
+      >
+        {new Array(6).fill(0).map((_, index) => {
+          const X = (D / 2) * (1 + Math.cos((index * (2 * Math.PI)) / 6));
+          const Y = (D / 2) * (1 - Math.sin((index * (2 * Math.PI)) / 6));
+
+          return (
+            <li
+              key={index}
+              className={`${styles.Point} ${currentPoint === index + 1 ? styles.active : ""}`}
+              style={{
+                top: Y,
+                left: X,
+                transform: `translate(-50%, -50%) rotate(${pointDeg}deg)`,
+              }}
+              onClick={() => onPointClick(index + 1)}
+            >
+              {index + 1}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default CentetCircle;
